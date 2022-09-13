7f39232ee527d28cf0e781566460bbe6d2016dcc
gekozen <76520530+gekozen@users.noreply.github.com>
2022-09-13 18:36:30 +0100

    Update namecheck.js

--- namecheck.js
+++ namecheck.js
@@ -1,4 +1,4 @@
-//take username from user
+//get username from user
 
 let savedName;
 	if (window.localStorage.getItem("savedName") === null){
@@ -6,10 +6,30 @@ let savedName;
 	}else {
 		savedName = JSON.parse(window.localStorage.getItem("savedName"));
 	}
+//If cancel button or no name is entered..
+if (savedName === null || savedName === "") {
+		savedName = "Mr. Nobody"
+	};
 
 window.localStorage.setItem("savedName", JSON.stringify(savedName));
 
 document.getElementById("todoName").innerText = savedName + "\'s todo list!"
 
+//Selectors
+const nameButton = document.querySelector(".reinputNameButton");
 
+//event listeners
+nameButton.addEventListener("click", funcSavedName);
+
+//functions
+function funcSavedName(){
+	savedName = prompt("Hi. What is your name?");
+	if (savedName === null || savedName === "") {
+		return;
+	}else {
+	window.localStorage.setItem("savedName", JSON.stringify(savedName));
+	document.getElementById("todoName").innerText = savedName + "\'s todo list!"
+	}
+	
+};
 
