7dd578082d09062ed92654e71e7a4c760aa0f256
gekozen <76520530+gekozen@users.noreply.github.com>
2022-09-13 20:11:38 +0100

    Update script.js

--- script.js
+++ script.js
@@ -3,12 +3,20 @@ const todoInput = document.querySelector(".todo-input");
 const todoButton = document.querySelector(".todo-button");
 const todoList = document.querySelector(".todo-list");
 const filterOption = document.querySelector(".filter-todo");
+const deleteList = document.querySelector(".deleteButton");
 
 //event listener
 document.addEventListener("DOMContentLoaded", getTodos);
 todoButton.addEventListener("click", addTodo);
 todoList.addEventListener("click", deleteCheck);
 filterOption.addEventListener("click", filterTodo);
+deleteList.addEventListener("click", funcDeleteList);
+
+function funcDeleteList() {
+	window.localStorage.removeItem("todos");
+	window.localStorage.removeItem("savedName");
+	location.reload();
+}
 
 
 function addTodo(event) {
